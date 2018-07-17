b519b274cbf5722628650d9c88f9d50ad28ec16b
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:57:59 +0200

    add owner which make a problem let's solve it

--- forum/resources/views/threads/show.blade.php
+++ forum/resources/views/threads/show.blade.php
@@ -15,7 +15,10 @@
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
                     <div class="card">
-                        <div class="card-header">{{ $reply->created_at->diffForHumans() }}</div>
+                        <div class="card-header">
+                            {{ $reply->owner->name }}
+                            {{ $reply->created_at->diffForHumans() }}
+                        </div>
                         <div class="card-body">{{ $reply->body }}</div>
                     </div>
                     <br>
