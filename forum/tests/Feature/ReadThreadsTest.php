bc74267e4e3f5ec67418b1867e1a4f9554ec6e72
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:25:14 +0200

    refactoring the code by make function setUp

--- forum/tests/Feature/ReadThreadsTest.php
+++ forum/tests/Feature/ReadThreadsTest.php
@@ -9,6 +9,20 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+//    private $thread;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+
+    }
 
 
 
@@ -24,9 +38,11 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_view_all_threads()
     {
         //test2 : if i make thread i can see the title of that thread
-        $thread = factory('App\Thread')->create();
+        //$thread = factory('App\Thread')->create();
         $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        //$response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
+
 
     }
 
@@ -35,9 +51,23 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_read_a_single_threads()
     {
         //test3 : user can show one thread
-        $thread = factory('App\Thread')->create();
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+//        $thread = factory('App\Thread')->create();
+//        $response = $this->get('/threads/' . $thread->id);
+//        $response->assertSee($thread->title);
+
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
+
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        // Given we have a thread
+        // And that thread includes replies
+        factory('App\Reply')->create(['thread_id'=>$this->thread->id]);
+        // When we visit a thread page
+        // Then we should see the replies
 
     }
 
