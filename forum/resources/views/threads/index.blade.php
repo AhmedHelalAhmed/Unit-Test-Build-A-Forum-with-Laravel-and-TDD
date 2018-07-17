74f46881e19360f660a6c22dab0a43b3b371fabc
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:02:52 +0200

    make method path in model to generate the url

--- forum/resources/views/threads/index.blade.php
+++ forum/resources/views/threads/index.blade.php
@@ -11,7 +11,7 @@
                 @foreach($threads as $thread)
                 <article>
                     <h4>
-                        <a href="/threads/{{ $thread->id  }}">{{ $thread->title }}</a>
+                        <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
                     </h4>
                     <vid class="body">{{ $thread->body }}</vid>
                 </article>
