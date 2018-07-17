88b8134d11a19a24a6cc04254adaa3de60a15a04
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:48:32 +0200

    add replies to a thread

--- forum/tests/Feature/ReadThreadsTest.php
+++ forum/tests/Feature/ReadThreadsTest.php
@@ -30,18 +30,16 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_browse_threads()
     {
         //test1 : response of the serve is 200 ok
-        $response = $this->get('/threads');
-        $response->assertStatus(200);
+        $this->get('/threads')
+            ->assertStatus(200);
     }
 
     /** @test */
     public function a_user_can_view_all_threads()
     {
         //test2 : if i make thread i can see the title of that thread
-        //$thread = factory('App\Thread')->create();
-        $response = $this->get('/threads');
-        //$response->assertSee($thread->title);
-        $response->assertSee($this->thread->title);
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
 
 
     }
@@ -51,12 +49,8 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_read_a_single_threads()
     {
         //test3 : user can show one thread
-//        $thread = factory('App\Thread')->create();
-//        $response = $this->get('/threads/' . $thread->id);
-//        $response->assertSee($thread->title);
-
-        $response = $this->get('/threads/' . $this->thread->id);
-        $response->assertSee($this->thread->title);
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
 
     }
 
@@ -65,13 +59,12 @@ class ReadThreadsTest extends TestCase
     {
         // Given we have a thread
         // And that thread includes replies
-        factory('App\Reply')->create(['thread_id'=>$this->thread->id]);
+        $reply = factory('App\Reply')
+            ->create(['thread_id'=>$this->thread->id]);
         // When we visit a thread page
         // Then we should see the replies
-
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
     }
-
-
-
 
 }
