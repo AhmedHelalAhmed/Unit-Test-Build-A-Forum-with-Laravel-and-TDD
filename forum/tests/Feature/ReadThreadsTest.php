eb81354fc7c79e21f61c0940ff543d4efa54c26a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:04:34 +0200

    start video 3

--- forum/tests/Feature/ReadThreadsTest.php
+++ forum/tests/Feature/ReadThreadsTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
