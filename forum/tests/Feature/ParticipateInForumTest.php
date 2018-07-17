78178cc1802a54b8100f6c7799b165deb1b3c42d
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 16:12:02 +0200

    clean up ParticipateInForumTest

--- forum/tests/Feature/ParticipateInForumTest.php
+++ forum/tests/Feature/ParticipateInForumTest.php
@@ -17,29 +17,25 @@ class ParticipateInForumTest extends TestCase
 
         $this->expectException('Illuminate\Auth\AuthenticationException');
 
-        $thread = factory('App\Thread')->create();
-
-        $reply = factory('App\Reply')->create();
-        $this->post($thread->path().'/replies',$reply->toArray());
+        $this->post('/threads/1/replies',[]);
     }
 
     /** @test */
     function an_authenticated_user_may_participate_in_form_threads()
     {
 
-        // Given we have an authenticated user
-        $user = factory('App\User')->create();
-        $this->signIn($user);//to make the user authenticated
 
-        // And an existing thread
+        $this->signIn($user = factory('App\User')->create());//to make the user authenticated
+
+
         $thread = factory('App\Thread')->create();
 
-        // when the user adds a reply to the thread
-        $reply = factory('App\Reply')->create();
+        // TODO : see the difference between make and create
+        $reply = factory('App\Reply')->make();
         $this->post($thread->path().'/replies',$reply->toArray());
 
 
-        // Then their reply should be visible on the page
+
         $this->get($thread->path())
             ->assertSee($reply->body);
     }
