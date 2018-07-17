013203936d381236b23d2d272e53dd05ec47cb6e
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 11:51:28 +0200

    make created at human readable

--- forum/resources/views/threads/show.blade.php
+++ forum/resources/views/threads/show.blade.php
@@ -15,7 +15,7 @@
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
                     <div class="card">
-                        <div class="card-header">{{ $reply->created_at }}</div>
+                        <div class="card-header">{{ $reply->created_at->diffForHumans() }}</div>
                         <div class="card-body">{{ $reply->body }}</div>
                     </div>
                     <br>
