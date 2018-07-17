b9aef33046a4a9055368b7fe57c41f8a127cecb4
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-17 12:33:08 +0200

    add unit test for reply and solve the owner issue

--- forum/app/Reply.php
+++ forum/app/Reply.php
@@ -7,5 +7,12 @@ use App\User;
 
 class Reply extends Model
 {
-    //
+
+    public function owner()
+    {
+        // we path the parameter to that
+        // make laravel use user_id not owner_id
+
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
